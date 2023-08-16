a3cf8a7103f1fe1f4403dd82071beb81c112e9e8
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2023-08-16 21:25:26 +0200

    Use public function to register built-in operators
    Document said function as well so it serves as an example on how to use

--- operator.go
+++ operator.go
@@ -19,79 +19,102 @@ import (
 //	}
 type Operator func(c Clause) (string, []any, error)
 
+// RegisterOperator registers an operator with the given name and function.
+// the name, given to the operator here, can be used to reference the operator
+// from the struct tag.
+
+// Note that calling this function multiple times with the same name will
+// overwrite the function previously registered to the operator without warning.
+//
+// Example:
+//
+//	RegisterOperator("eq", func()...)
+//
+//	type Filter struct {
+//	    Price int `filter:"price,op=eq"`
+//	                                ^^--- operator name
+//	}
+func RegisterOperator(name string, op Operator) {
+	Operators[name] = op
+}
+
 func init() {
-	Operators = map[string]Operator{
-		"eq":  SimpleOperator("= ?"),
-		"gt":  SimpleOperator("> ?"),
-		"gte": SimpleOperator(">= ?"),
-		"lte": SimpleOperator("<= ?"),
-		"lt":  SimpleOperator("< ?"),
-		"in": func(c Clause) (string, []any, error) {
-			if err := c.AssertTypeOneOf(reflect.Slice, reflect.Array); err != nil {
-				return "", nil, err
-			}
+	// register built in operators
+	RegisterOperator("eq", SimpleOperator("= ?"))
+	RegisterOperator("gt", SimpleOperator("> ?"))
+	RegisterOperator("gte", SimpleOperator(">= ?"))
+	RegisterOperator("lte", SimpleOperator("<= ?"))
+	RegisterOperator("lt", SimpleOperator("< ?"))
 
-			// early return when passed slice is empty
-			if c.reflectedValue.Len() == 0 {
-				return "IN(NULL)", []any{}, nil
-			}
+	RegisterOperator("in", func(c Clause) (string, []any, error) {
+		if err := c.AssertTypeOneOf(reflect.Slice, reflect.Array); err != nil {
+			return "", nil, err
+		}
 
-			placeholders := PlaceholderList(c.reflectedValue.Len())
-			elems, err := readSliceElems(c.reflectedValue)
+		// early return when passed slice is empty
+		if c.reflectedValue.Len() == 0 {
+			return "IN(NULL)", []any{}, nil
+		}
 
-			if err != nil {
-				return "", nil, err
-			}
-			return fmt.Sprintf("IN(%s)", placeholders), elems, nil
-		},
-		"not-in": func(c Clause) (string, []any, error) {
-			if err := c.AssertTypeOneOf(reflect.Slice, reflect.Array); err != nil {
-				return "", nil, err
-			}
+		placeholders := PlaceholderList(c.reflectedValue.Len())
+		elems, err := readSliceElems(c.reflectedValue)
 
-			// early return when passed slice is empty
-			if c.reflectedValue.Len() == 0 {
-				return "NOT IN(NULL)", []any{}, nil
-			}
+		if err != nil {
+			return "", nil, err
+		}
+		return fmt.Sprintf("IN(%s)", placeholders), elems, nil
+	})
 
-			placeholders := PlaceholderList(c.reflectedValue.Len())
-			elems, err := readSliceElems(c.reflectedValue)
-			if err != nil {
-				return "", nil, err
-			}
-			return fmt.Sprintf("NOT IN(%s)", placeholders), elems, nil
-		},
-		"between": func(c Clause) (string, []any, error) {
-			if err := c.AssertTypeOneOf(reflect.Slice, reflect.Array); err != nil {
-				return "", nil, err
-			}
+	RegisterOperator("not-in", func(c Clause) (string, []any, error) {
+		if err := c.AssertTypeOneOf(reflect.Slice, reflect.Array); err != nil {
+			return "", nil, err
+		}
 
-			if c.reflectedValue.Len() < 2 {
-				return "", nil, fmt.Errorf("operation between expects two elements in its slice")
-			}
+		// early return when passed slice is empty
+		if c.reflectedValue.Len() == 0 {
+			return "NOT IN(NULL)", []any{}, nil
+		}
 
-			elems, err := readSliceElems(c.reflectedValue)
-			if err != nil {
-				return "", nil, err
-			}
+		placeholders := PlaceholderList(c.reflectedValue.Len())
+		elems, err := readSliceElems(c.reflectedValue)
+		if err != nil {
+			return "", nil, err
+		}
+		return fmt.Sprintf("NOT IN(%s)", placeholders), elems, nil
+	})
 
-			return "BETWEEN ? AND ?", elems[:2], nil
-		},
-		"is-null": func(c Clause) (string, []any, error) {
-			if c.reflectedValue.Bool() {
-				return "IS NULL", []any{}, nil
-			}
+	RegisterOperator("between", func(c Clause) (string, []any, error) {
+		if err := c.AssertTypeOneOf(reflect.Slice, reflect.Array); err != nil {
+			return "", nil, err
+		}
 
-			return "IS NOT NULL", []any{}, nil
-		},
-		"not-null": func(c Clause) (string, []any, error) {
-			if c.reflectedValue.Bool() {
-				return "IS NOT NULL", []any{}, nil
-			}
+		if c.reflectedValue.Len() < 2 {
+			return "", nil, fmt.Errorf("operation between expects two elements in its slice")
+		}
 
+		elems, err := readSliceElems(c.reflectedValue)
+		if err != nil {
+			return "", nil, err
+		}
+
+		return "BETWEEN ? AND ?", elems[:2], nil
+	})
+
+	RegisterOperator("is-null", func(c Clause) (string, []any, error) {
+		if c.reflectedValue.Bool() {
 			return "IS NULL", []any{}, nil
-		},
-	}
+		}
+
+		return "IS NOT NULL", []any{}, nil
+	})
+
+	RegisterOperator("not-null", func(c Clause) (string, []any, error) {
+		if c.reflectedValue.Bool() {
+			return "IS NOT NULL", []any{}, nil
+		}
+
+		return "IS NULL", []any{}, nil
+	})
 }
 
 // SimpleOperator is a shorthand function for creating operators with a one-to-one matching
